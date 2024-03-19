6b63408ce3de9fde0c72e8b43138a580d73e0226
DESKTOP-TAIH814\Lenovo <damian.luis.porta@gmail.com>
2024-03-18 23:52:06 -0300

    added yml file

--- routes/index.ts
+++ routes/index.ts
@@ -2,6 +2,6 @@ import express from 'express'
 import { getUser } from '../controllers'
 const Router=express.Router()
 
-Router.get('/getUser/:id',getUser)
+Router.get('/getUser',getUser)
 
 export default Router
---
+++ tests/api.test.js
@@ -0,0 +1,20 @@
+// test/sum.test.js
+
+function sum(a, b) {
+    return a + b;
+  }
+  
+  describe('Prueba de la función sum', () => {
+    test('Suma de 1 + 2 igual a 3', () => {
+      expect(sum(1, 2)).toBe(3);
+    });
+  
+    test('Suma de -1 + 1 igual a 0', () => {
+      expect(sum(-1, 1)).toBe(0);
+    });
+  
+    test('Suma de 0 + 0 igual a 0', () => {
+      expect(sum(0, 0)).toBe(0);
+    });
+  });
+  
